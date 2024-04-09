0fe794bdf69b873bd1be2a40b872560f3cc9fc00
dbakare2 <dmbakare@gmail.com>
2024-04-09 12:48:45 +0100

    a description of your changes here

--- main.py
+++ main.py
@@ -1,32 +1,53 @@
 def main():
-    book_path = "../Frankenstein.txt"
-    #"github.com/dbakare2/bookbot/books/Frankenstein.txt"
+    book_path = "books/Frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = word_counter(text)
-    print(text)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
     print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
 
 def get_book_text(path):
-# gets the text from the book in the specified path
     with open(path) as f:
-    # A 'with' block to open the file
         return f.read()
-    # reads the contents of the file
 
-def word_counter(text):
-    words = text.split()
-    book_words = len(words)
-    return book_words
-
-def letter_counter(text):
-    l_count = {}
-    for letter in text:
-        l_lower = letter.lower()
-        if l_lower in l_count:
-            l_count[l_lower] += 1
-        else:
-            l_count[l_lower] = 1
-    return l_count
-            #l_count.append({'l_string':'count.l_string'})
 
 main()
